def16aff387e2f99497cdd54c89798c36e3d6115
James <jybryce4@gmail.com>
2021-01-07 11:15:24 -0600

    created login service and model

--- Bloggo/Services/LoginService.cs
+++ Bloggo/Services/LoginService.cs
@@ -1,7 +1,20 @@
+using Bloggo.Models;
+
 namespace Bloggo.Services
 {
     public class LoginService
     {
-        
+        private UserDatabaseService _userDatabaseService;
+        public User User { get; private set; }
+
+        public LoginService(UserDatabaseService userDatabaseService)
+        {
+            _userDatabaseService = userDatabaseService;
+        }
+
+        public void Login(Login model)
+        {
+            
+        }
     }
 }
